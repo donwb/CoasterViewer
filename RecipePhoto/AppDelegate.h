44571a5d18bb6b879b079a6e2bf2175c5ec3b300
Don Browning <don.browning@gmail.com>
2013-11-15 13:41:49 -0500

    Initial Commit

---
+++ RecipePhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RecipePhoto
+//
+//  Created by Don Browning on 11/15/13.
+//  Copyright (c) 2013 Don Browning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RecipePhoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RecipePhoto
+//
+//  Created by Don Browning on 11/15/13.
+//  Copyright (c) 2013 Don Browning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
