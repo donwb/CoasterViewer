a8590b8e12abca1268eab1502865707fd033d144
don bronwing <don.browning@gmail.com>
2013-11-27 10:11:37 -0500

    finally got device rotation figured out (sort of)

--- RecipePhoto/DetailViewController.m
+++ RecipePhoto/DetailViewController.m
@@ -11,6 +11,7 @@
 @interface DetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *photoName;
 @property (weak, nonatomic) IBOutlet UIImageView *photo;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -28,11 +29,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // Device Orientation notifications
+    [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationChanged:) name:UIDeviceOrientationDidChangeNotification object:nil];
+    
 	// Do any additional setup after loading the view.
     
     self.photoName.text = self.selectedImage.name;
     self.photo.image = [UIImage imageNamed:self.selectedImage.filename];
     
+}
+
+- (void)orientationChanged:(NSNotification *)notification {
+    UIInterfaceOrientation current = [UIApplication sharedApplication].statusBarOrientation;
+    if(current == UIDeviceOrientationPortrait)
+    {
+        NSLog(@"Portrait");
+        // seems to default back to the way it's designed in the storyboard
+    } else {
+        NSLog(@"Landscape");
+        CGRect frame = self.imageView.frame;
+        frame.origin.x = 190;
+        frame.origin.y = 70;
+        frame.size.height = 230;
+        frame.size.width = 230;
+        self.imageView.frame = frame;
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,4 +64,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewDidDisappear:(BOOL)animated{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+}
 @end
